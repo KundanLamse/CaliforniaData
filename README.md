84ad774b100a28f8548c7632f263ac21984ef0f2
Kundan Lamse <kundanlamse19@gmail.com>
2024-08-05 21:07:58 +0530

    Final Web

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # CaliforniaData
+# Software And Tools used   
+1. GitHub
+2. VS CODE IDE 
+3. Heroku
+4. GitCLI
+
+# Create new env called venv1
--- app.py
+++ app.py
@@ -12,7 +12,6 @@ def home():
     return render_template('home.html')
 
 @app.route('/predict_api',methods=['POST'])
-
 def predict_api():
     data=request.json['data']
     print(data)
@@ -22,6 +21,16 @@ def predict_api():
     output=redmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x)for x in request.form.values()]
+    final_input=scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=redmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="Predicted Price is {}".format(output))
+
 
 if __name__=="__main__":
     app.run(debug=True)
